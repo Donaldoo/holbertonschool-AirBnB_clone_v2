9103f11f8b263c994f198af105c7ff1ebbd32980
Donaldoo <donald.kumaraku@gmail.com>
2022-11-23 13:23:12 -0800

    dbstorage test

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Testing the console module """
+
+import unittest
+from models.engine import db_storage
+from models.engine.db_storage import DBStorage
+
+
+class TestConsole(unittest.TestCase):
+    """ Test class for DBStorage """
+
+    def test_documentation(self):
+        """ Test docstrings documentation"""
+
+        self.assertTrue(db_storage.__doc__)
+        self.assertTrue(db_storage.DBStorage.__doc__)
+
+    def test_methods_doc(self):
+        """ Test all docstrings of each method"""
+
+        for all_methods in dir(DBStorage):
+            self.assertTrue(all_methods.__doc__)
+
+
+if __name__ == '__main__':
+    unittest.main()
