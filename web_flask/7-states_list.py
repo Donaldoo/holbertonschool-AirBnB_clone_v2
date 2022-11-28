9fbe4b422ad621f5aec75a5caf9581a71c705a59
Donaldoo <donald.kumaraku@gmail.com>
2022-11-28 12:50:05 -0800

    Task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web app """
+
 from models import storage
 from flask import Flask, render_template
 from models.state import State
@@ -9,17 +10,15 @@ app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def states():
-    """ displays the states """
+def states_list():
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown(self):
-    """ removes current sqlalchemy session """
     storage.close()
 
 
-if __name__ == '__main__':
+if __name__ = '__main__':
     app.run(host='0.0.0.0', port=5000)
