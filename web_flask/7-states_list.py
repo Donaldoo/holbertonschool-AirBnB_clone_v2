6cf167ac8bfb14b1944e9f7ad0224de617f02c94
Donaldoo <donald.kumaraku@gmail.com>
2022-11-28 13:06:47 -0800

    Task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web app """
+""" Starts a Flask web app """
+
 
 from models import storage
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ displays states """
-    states = storage.all('State').values()
-    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
@@ -21,5 +14,12 @@ def teardown(self):
     storage.close()
 
 
-if __name__ = '__main__':
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ displays states """
+    states = storage.all("State").values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
