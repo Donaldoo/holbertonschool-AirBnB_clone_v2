75ef4bfc3537b30f18d4625fc0ea3247ea756719
Donaldoo <donald.kumaraku@gmail.com>
2022-11-29 10:25:22 -0800

    task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -19,10 +19,11 @@ def states():
 @app.route('/states/<id>', strict_slashes=False)
 def states_id(id):
     """ displays states """
-    for state in storage.all(State).values:
+    states = storage.all(State).values()
+    for state in states:
         if state.id == id:
-            return render_template('9-states.html', state=state)
-    return render_tempalte('9-states.html')
+            return render_template('9-states.html', states=state, id=state.id)
+    return render_tempalte('9-states.html', states=state, id='none')
 
 
 @app.teardown_appcontext
