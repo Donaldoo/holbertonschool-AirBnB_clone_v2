f1ad07b92f3a2e73ba3407dbb3ccda6c8ebc0df5
Donaldoo <donald.kumaraku@gmail.com>
2022-11-29 15:19:14 -0800

    update

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -23,7 +23,7 @@ def states_id(id):
     for state in states:
         if state.id == id:
             return render_template('9-states.html', states=state, id=state.id)
-    return render_tempalte('9-states.html', states=state)
+    return render_tempalte('9-states.html')
 
 
 @app.teardown_appcontext
