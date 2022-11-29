12024ccbb94b59ac4cdfd879d1ba0dd70cad8f7c
Donaldoo <donald.kumaraku@gmail.com>
2022-11-29 10:29:42 -0800

    Task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 def states():
     """ fetching data """
     states = storage.all(State).values()
-    return render_template('9-states.html', states=states)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.route('/states/<id>', strict_slashes=False)
